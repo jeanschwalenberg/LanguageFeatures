ed87f48f4724752a94e16034bfd38916314393b6
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-01-12 13:54:45 -0700

    refactors

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -12,5 +12,19 @@ namespace LanguageFeatures.Controllers
         public string Index() {
             return "Navigate to a URL to show an example.";
         }
+
+        public ViewResult AutoProperty() {
+            //create a new Product object
+            Product myProduct = new Product();
+
+            //set the property value
+            myProduct.Name = "Kayak";
+
+            //get the property
+            string productName = myProduct.Name;
+
+            //generate the result
+            return View("Result", (object)String.Format("Product name: {0}", productName));
+        }
     }
 }
