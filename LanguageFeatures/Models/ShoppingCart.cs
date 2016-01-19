6a006933607e97ef5cdcc3b71cf46c501c23a370
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-01-19 16:17:03 -0700

    Added ShoppingCart.cs extension method.

---
+++ LanguageFeatures/Models/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models {
+    public class ShoppingCart: IEnumerable<Product> {
+        public List<Product> Products { get; set; }
+        public IEnumerator<Product> GetEnumerator() {
+            return Products.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+    }
+}
